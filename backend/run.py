1a2e99d9017b47e88d8b59ab70daa185cc01642b
Abel <se.abel.tadesse@gmail.com>
2025-05-24 17:25:40 +0300

    fix: add CORS support for the application

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,10 @@
 from app import create_app
 from flasgger import Swagger
+from flask_cors import CORS
 
 app = create_app()
 swagger = Swagger(app)
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
